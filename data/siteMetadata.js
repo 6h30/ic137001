3af746ceefce1b90cfaf0d2880035a496bfef633
6h30 <truongduc1108@gmail.com>
2025-07-10 11:55:15 +0000

    update

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,9 +1,9 @@
 /** @type {import("pliny/config").PlinyConfig } */
 const siteMetadata = {
-  title: 'Next.js Starter Blog',
-  author: 'Tails Azimuth',
-  headerTitle: 'TailwindBlog',
-  description: 'A blog created with Next.js and Tailwind.css',
+  title: 'DcViet Blog',
+  author: 'DucTruong',
+  headerTitle: 'DcViet Blog',
+  description: 'Revit API and C#',
   language: 'en-us',
   theme: 'system', // system, dark or light
   siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
