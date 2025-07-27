8e363da30fc8f2de0b15a3460df4859f851e6ca1
DcViet <tranducviet.eng@gmail.com>
2025-07-27 23:13:52 +0700

    update new layout3

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,32 +1,7 @@
-// import { allCoreContent, sortPosts } from 'pliny/utils/contentlayer'
-// import { allBlogs } from 'contentlayer/generated'
-// import { genPageMetadata } from 'app/seo'
-// import ListLayout from '@/layouts/ListLayoutWithTags'
-
-// export const metadata = genPageMetadata({ title: 'Blog' })
-
-// export default async function BlogPage({
-//   searchParams,
-// }: {
-//   searchParams?: { tag?: string; page?: string }
-// }) {
-//   const selectedTags = searchParams?.tag?.split(',') ?? []
-
-//   const allPosts = sortPosts(allBlogs)
-
-//   const filteredPosts =
-//     selectedTags.length > 0
-//       ? allPosts.filter((post) => selectedTags.every((tag) => post.tags?.includes(tag)))
-//       : allPosts
-
-//   const posts = allCoreContent(filteredPosts)
-
-//   return <ListLayout posts={posts} title="Tất cả bài viết" />
-// }
-
 import { allCoreContent, sortPosts } from 'pliny/utils/contentlayer'
 import { allBlogs } from 'contentlayer/generated'
 import { genPageMetadata } from 'app/seo'
+import { Suspense } from 'react'
 import ListLayout from '@/layouts/ListLayoutWithTags'
 
 export const metadata = genPageMetadata({ title: 'Blog' })
@@ -35,5 +10,9 @@ export default function BlogPage() {
   const allPosts = sortPosts(allBlogs)
   const posts = allCoreContent(allPosts)
 
-  return <ListLayout posts={posts} title="Tất cả bài viết" />
+  return (
+    <Suspense fallback={<div>Loading blog...</div>}>
+      <ListLayout posts={posts} title="Tất cả bài viết" />
+    </Suspense>
+  )
 }
