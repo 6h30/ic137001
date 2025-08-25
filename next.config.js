9c09e19785a616335e632dc08e52300a05e69ebd
tranducviet.eng <tranducviet.eng@gmail.com>
2025-08-25 06:44:55 +0000

    update cms2

--- next.config.js
+++ next.config.js
@@ -30,7 +30,7 @@ const ContentSecurityPolicy = `
   script-src-elem 'self' 'unsafe-inline' 'unsafe-eval' https://cdn.jsdelivr.net https://unpkg.com https://pagead2.googlesyndication.com https://googleads.g.doubleclick.net https://ep2.adtrafficquality.google https://analytics.umami.is https://www.googletagmanager.com https://fpyf8.com https://vaimucuvikuwu.net https://e2ertt.com giscus.app https://api.github.com blob:;
   worker-src 'self' blob:;
   style-src 'self' 'unsafe-inline';
-  img-src 'self' blob: data: https://*.google.com https://*.googlesyndication.com https://*.gstatic.com https://ep1.adtrafficquality.google https://images.spiderum.com https://lh3.googleusercontent.com;
+  img-src 'self' blob: data: https://*.google.com https://*.googlesyndication.com https://*.gstatic.com https://ep1.adtrafficquality.google https://images.spiderum.com https://lh3.googleusercontent.com https://avatars.githubusercontent.com;
   font-src 'self' https://fonts.gstatic.com;
   connect-src 'self' 'unsafe-eval' https://unpkg.com https://cdn.jsdelivr.net https://ep1.adtrafficquality.google https://ep2.adtrafficquality.google https://pagead2.googlesyndication.com https://www.google.com https://*.googlesyndication.com https://tpc.googlesyndication.com https://analytics.umami.is https://*.google-analytics.com https://www.googletagmanager.com https://fpyf8.com https://vaimucuvikuwu.net https://e2ertt.com giscus.app https://api.github.com;
   media-src 'self' *.s3.amazonaws.com;;
